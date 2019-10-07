32a0fff9c56f3ab36014e7c32d80e7571a2711e4
raymag <prayk11@outlook.com>
2019-10-07 00:00:13 -0300

    Add speed const

--- src/server.js
+++ src/server.js
@@ -1,68 +1,69 @@
-const express = require('express')();
-const http = require('http').createServer(express);
-const io = require('socket.io')(http);
+const express = require("express")();
+const http = require("http").createServer(express);
+const io = require("socket.io")(http);
 
-const app = require('express');
+const app = require("express");
 
 let players = {};
 
-function gameUpdate(){
-    io.emit('gameUpdate', players);
+function gameUpdate() {
+  io.emit("gameUpdate", players);
 }
-function playerStart(socket){
-    players[socket.id] = {
-        x: 0,
-        y: 0
-    };
-    socket.emit('start', players);
+function playerStart(socket) {
+  players[socket.id] = {
+    x: 0,
+    y: 0
+  };
+  socket.emit("start", players);
 }
-function playerMove(socket, move){
-    switch(move){
-        case 'up':
-            if(players[socket.id]['y']>=10){
-                players[socket.id]['y'] -= 10;
-            }
-            break;
-        case 'down':
-            if(players[socket.id]['y']<540){
-                players[socket.id]['y'] += 10;
-            }
-            break;
-        case 'left':
-            if(players[socket.id]['x']>=10){
-                players[socket.id]['x'] -= 10;
-            }
-            break;
-        case 'right':
-            if(players[socket.id]['x']<540){
-                players[socket.id]['x'] += 10;
-            }
-            break;
-    }
-    gameUpdate();
+function playerMove(socket, move) {
+  const speed = 60;
+  switch (move) {
+    case "up":
+      if (players[socket.id]["y"] >= speed) {
+        players[socket.id]["y"] -= speed;
+      }
+      break;
+    case "down":
+      if (players[socket.id]["y"] < 600 - speed) {
+        players[socket.id]["y"] += speed;
+      }
+      break;
+    case "left":
+      if (players[socket.id]["x"] >= speed) {
+        players[socket.id]["x"] -= speed;
+      }
+      break;
+    case "right":
+      if (players[socket.id]["x"] < 600 - speed) {
+        players[socket.id]["x"] += speed;
+      }
+      break;
+  }
+  gameUpdate();
 }
 
-express.use('/static', app.static(__dirname + '/assets/'));
+express.use("/static", app.static(__dirname + "/assets/"));
 
-express.get('/', (req, res) => {
-    res.sendFile(__dirname+'/views/index.html');
+express.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-io.on('connection', (socket) => {
-    playerStart(socket);
-    console.log(`User ${socket.id} connected'`);
-    socket.on('disconnect', () => {
-        delete players[socket.id];
-        gameUpdate(players);
-        console.log(`User ${socket.id} disconnected`);
-    });
-    socket.on('playerMove', (move) => {
-        playerMove(socket, move);
-    });
+io.on("connection", socket => {
+  playerStart(socket);
+  console.log(`User ${socket.id} connected'`);
+  socket.on("disconnect", () => {
+    delete players[socket.id];
+    gameUpdate(players);
+    console.log(`User ${socket.id} disconnected`);
+  });
+  socket.on("playerMove", move => {
+    playerMove(socket, move);
+  });
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 http.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
+  console.log(`Listening on port ${PORT}`);
 });
